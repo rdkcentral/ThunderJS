b2363eb15012c800eff7bacd00df97458e559185
Stefan Verkoyen <stefan.verkoyen@gmail.com>
2021-08-27 12:04:48 +0200

    Add connection test for two different thunderjs instances

--- tests/thunderJS.1connect.spec.js
+++ tests/thunderJS.1connect.spec.js
@@ -24,15 +24,22 @@ import ThunderJS from '../src/thunderJS'
 import sinon from 'sinon'
 
 const port = 2021
+const port2 = 2022
+let messages = {}
 
-const startServer = () => {
+const startServer = (port_) => {
   // create a websocket server
   const server = new Websocket.Server({
-    port,
+    port: port_,
   })
 
   server.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {})
+    ws.on('message', function incoming(message) {
+      messages[port_] = message
+      message = JSON.parse(message)
+      message.result = 'some result for '+message.method
+      ws.send(JSON.stringify(message))
+    })
   })
 
   return server
@@ -40,7 +47,7 @@ const startServer = () => {
 
 // on connect event should be called when ws connection is established
 test('thunderJS - connect - callback when opening connection', assert => {
-  const server = startServer()
+  const server = startServer(port)
 
   const thunderJS = ThunderJS({
     host: 'localhost',
@@ -74,6 +81,68 @@ test('thunderJS - connect - callback when opening connection', assert => {
         1,
         'Disconnect callback should be called once after connection is closed'
       )
+      assert.end()
+    }, 1000)
+  }, 1000)
+})
+
+// different instances should support connections to different servers
+test('thunderJS - connect - support connections to different servers', assert => {
+  const server = startServer(port)
+  const server2 = startServer(port2)
+
+  const thunderJS = ThunderJS({
+    host: 'localhost',
+    port,
+    endpoint: '/',
+  })
+
+  // make a dummy API call
+  let reply1 = {}
+  thunderJS.call('Foo', 'bar').then(result => {
+    reply1 = result
+  }).catch(err => {
+    assert.fail('Call Foo.1.bar() failed')
+  })
+
+  // thunderJS instance for server2
+  const thunderJS2 = ThunderJS({
+    host: 'localhost',
+    port: port2,
+    endpoint: '/endpoint2',
+  })
+
+  // make a dummy API call
+  let reply2 = {}
+  thunderJS2.call('Foo2', 'bar2').then(result => {
+    reply2 = result
+  }).catch(err => {
+    assert.fail('Call Foo2.1.bar2() failed')
+  })
+
+  // give it some time to execute the callback
+  setTimeout(() => {
+    assert.match(messages[port] || '', /Foo.1.bar/, 'Server1 should receive the message')
+    assert.equals(reply1, 'some result for Foo.1.bar', 'Result of Foo.1.bar() OK')
+    assert.match(messages[port2] || '', /Foo2.1.bar2/, 'Server2 should receive the message')
+    assert.equals(reply2, 'some result for Foo2.1.bar2', 'Result of Foo2.1.bar2() OK')
+
+    // make a call to server1 again
+    let reply3 = {}
+    thunderJS.call('Foox', 'dummy').then(result => {
+      reply3 = result
+    }).catch(err => {
+      assert.fail('Call Foox.1.dummy() failed')
+    })
+
+    setTimeout(() => {
+      assert.match(messages[port] || '', /Foox.1.dummy/, 'Server1 should receive the third message')
+      assert.equals(reply3, 'some result for Foox.1.dummy', 'Result of Foox.1.dummy() OK')
+
+      // close the websocket
+      server.close()
+      server2.close()
+
       assert.end()
     }, 1000)
   }, 1000)
